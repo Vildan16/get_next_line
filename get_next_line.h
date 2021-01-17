3134efb9360266a7b6eeb5c42fb2b60e37c29a0a
ameta <ameta@student.21-school.ru>
2021-01-17 15:02:01 +0300

    start

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ameta <ameta@student.21-school.ru>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/17 12:20:07 by ameta             #+#    #+#             */
+/*   Updated: 2021/01/17 12:33:29 by ameta            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int get_next_line(int fd, char **line);
+
+#endif
--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include "get_next_line.h"
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
 
 int main(void)
 {
     int fd = open("file", O_RDONLY);
+    char *line;
+    int i;
+    while ((i = get_next_line(fd, &line)))
+    {
+        printf("i = %d %s\n", i, line);
+        free(line);
+    }
+    //printf("i = %d %s\n", i, line);
+    //free(line);
+    close(fd);
+    return (0);
 }
